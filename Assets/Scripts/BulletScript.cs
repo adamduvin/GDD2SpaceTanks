6e827b0d76037c97d9e07b3abef9d3afeb5bc3bb
adamduvin <axd3010@g.rit.edu>
2018-02-26 23:28:10 -0500

    Gravity
    Added gravity and modified shoot to be less hardcoded. Added properties script for obstacles. Likely will have more things to store in there, otherwise I'll find an alternative later.

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class BulletScript : MonoBehaviour {
 
 
-    public Vector2 speed = new Vector2(10, 10);
-    public Vector2 direction = new Vector2(1, 0);
+    public Vector2 speed;
+    public Vector2 direction;
+    public float mass;
+    public float maxSpeed;
 
     private Vector2 movement;
     private Rigidbody2D rigidbodyComponent;
 
+    GameObject manager;
+    GravityManager gravMngr;
+
     // Use this for initialization
     void Start () {
-
-        
+        speed = new Vector2(1, 1);
+        speed = speed.normalized;
+        direction = new Vector2(1, 1);
+        direction = direction.normalized;
+        manager = GameObject.Find("GameManager");
+        gravMngr = manager.GetComponent<GravityManager>();
+        gravMngr.AssignProjectile(gameObject);
+        mass = 1.0f;
+        maxSpeed = 15.0f;
 
     }
 	
@@ -26,16 +38,25 @@ public class BulletScript : MonoBehaviour {
           speed.x * direction.x,
           speed.y * direction.y);
 
+        
+
         if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+
+        movement += gravMngr.MoveProjectile();      // Adds gravity
+        movement = movement.normalized;     // Normalize velocity vector
+        movement *= maxSpeed;   // Multiply velocity by max speed to get movement speed
 
         // Apply movement to the rigidbody
         rigidbodyComponent.velocity = movement;
+
+        direction = movement.normalized;    // Set direction as normal of velocity so it continues to move in the same direction
     }
 
   
 
     private void OnBecameInvisible()
     {
+        gravMngr.NullProjectile();
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/GravityManager.cs
+++ Assets/Scripts/GravityManager.cs
@@ -4,13 +4,68 @@ using UnityEngine;
 
 public class GravityManager : MonoBehaviour {
 
+    public GameObject projectile;       // Player's projectile
+    Rigidbody2D rigidbodyComponent;     // Projectile's rigid body 
+    CircleCollider2D boundingCircle;    // Projectile's bounding circle
+    public GameObject[] obstacles = new GameObject[1];      // List of obstacles
+    public float gravConst;     // Gravity constant
+
 	// Use this for initialization
 	void Start () {
-		
+        gravConst = 0.12f;      // Set gravity constant
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    // Calculates the force of gravity
+    Vector2 CalcGravity(float massProj, float massObst, float distance, Vector2 obstPos)
+    {
+        float force = (gravConst * massProj * massObst) / (distance * distance);
+        Vector2 forceGrav = new Vector2((obstPos.x - projectile.transform.position.x), (obstPos.y - projectile.transform.position.y));
+        forceGrav *= force;
+        return forceGrav;
+    }
+
+    public Vector2 MoveProjectile()
+    {
+        // If projectile exists, find the velocity caused by gravity and return it as a Vector2
+        if (projectile != null)
+        {
+            for (int i = 0; i < obstacles.Length; i++)
+            {
+                Vector2 projPosition = projectile.transform.position;
+                Vector2 obstPosition = obstacles[i].transform.position;
+                Vector2 distance = projPosition - obstPosition;
+                if (distance.magnitude < boundingCircle.radius + obstacles[i].GetComponent<CircleCollider2D>().radius)
+                {
+                    Vector2 gravity = CalcGravity(projectile.GetComponent<BulletScript>().mass, obstacles[i].GetComponent<ObstacleProperties>().mass, distance.magnitude, obstacles[i].transform.position);
+                    Vector2 acceleration = gravity / projectile.GetComponent<BulletScript>().mass;
+                    Vector2 velocity = rigidbodyComponent.velocity;
+                    velocity += acceleration;
+                    return velocity;
+                }
+            }
+        }
+        return Vector2.zero;
+    }
+    
+    // As a projectile is made, it calls this method to let the manager know it exists
+    public void AssignProjectile(GameObject projectile)
+    {
+        this.projectile = projectile;
+        rigidbodyComponent = projectile.GetComponent<Rigidbody2D>();
+        boundingCircle = projectile.GetComponent<CircleCollider2D>();
+    }
+
+    // As a projectile destroys itself, it calls this method to let the manager know it's gone
+    public void NullProjectile()
+    {
+        projectile = null;
+    }
 }
+
+
+// Man, I really missed C#
