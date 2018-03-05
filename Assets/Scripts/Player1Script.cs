05e8b26be6b26e56424c030856d535b8fbbef8ee
Ulijah Charles <ulijah96@hotmail.com>
2018-03-05 07:18:49 -0500

    Turn added and Shooting improved

--- Assets/Scripts/Player1Script.cs
+++ Assets/Scripts/Player1Script.cs
@@ -1,24 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 
 public class Player1Script : MonoBehaviour {
     public GameObject bullet;
+    public GameObject player2;
+    [SerializeField]
+    private Text turn;
+    public bool myturn;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        myturn = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         var r2d = GetComponent("Rigidbody2D");
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (myturn == true)
         {
-            Instantiate(bullet,transform.position,Quaternion.identity);
-        }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                var target = GameObject.Find("TargetBox1");
+                Instantiate(bullet, transform.position, transform.rotation);
+                turn.text = "Turn: Player 2";
+                myturn = false;
+                player2.GetComponent<Player2Script>().myturn = !false;
 
+            }
+        }
     }
 }
--- Assets/Scripts/Player2Script.cs
+++ Assets/Scripts/Player2Script.cs
@@ -1,24 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player2Script : MonoBehaviour {
     public GameObject bullet;
+    public GameObject player1;
+    [SerializeField]
+    private Text turn;
+    public bool myturn;
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        myturn = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
         var r2d = GetComponent("Rigidbody2D");
-        if (Input.GetKeyDown(KeyCode.KeypadEnter))
-        {
-            Instantiate(bullet, transform.position, Quaternion.identity);
-        }
+        if (myturn == true)
+            if (Input.GetKeyDown(KeyCode.KeypadEnter))
+            {
+                Instantiate(bullet, transform.position, Quaternion.identity);
+                turn.text = "Turn: Player 1";
+                myturn = false;
+                player1.GetComponent<Player1Script>().myturn = true;
+
+
+            }
+    }
 
     }
 
 
-}
+
