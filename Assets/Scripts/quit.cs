22dfcd37be8f2379cceef9210b86deb95ab446fc
Adam Duvin (RIT Student) <axd3010@ad.rit.edu>
2018-03-06 15:12:24 -0500

    Integrated Main Menu

---
+++ Assets/Scripts/quit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Quits the application upon pressing the appropriate button
+/// </summary>
+public class quit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Escape)){
+			Application.Quit();
+		}
+	}
+
+}
---
+++ Assets/Scripts/startButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class startButton : MonoBehaviour {
+
+	public Button start;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Button btn = start.GetComponent<Button> ();
+		btn.onClick.AddListener (StartGame);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void StartGame()
+	{
+		Application.LoadLevel ("game");
+		Debug.Log ("start");
+	}
+}
